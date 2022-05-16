c565421285a0bf3edc0c4637de52efe2dda7172a
Matheus Maniero <matheusmaniero@gmail.com>
2022-05-16 11:14:41 -0300

    Position

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position p1 = new Position(6,9);
+		System.out.println(p1);
+
+	}
+
+}
