8230e329480b810091ca94e3875e6a8a0d7ca48f
Matheus Maniero <matheusmaniero@gmail.com>
2022-05-17 10:55:45 -0300

    BoardException and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,5 +1,7 @@
 package boardgame;
 
+import chess.ChessPiece;
+
 public class Board {
 
 	private int rows;
@@ -10,7 +12,7 @@ public class Board {
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[rows][columns];
-		
+
 	}
 
 	public Piece[][] getPieces() {
@@ -33,35 +35,42 @@ public class Board {
 		this.columns = columns;
 	}
 
-	public Piece piece(int row, int column) {
-		return pieces[row][column];
+	public Piece piece(int row, int column) throws BoardException {
+		if (row >=0 && row < this.rows && column >= 0 && column < this.columns) {
+			return pieces[row][column];
+		}
+		throw new BoardException("This position exceeds the board.");
 	}
 
-	public Piece piece(Position position) {
-		return pieces[position.getRow()][position.getColumn()];
+	public Piece piece(Position position) throws BoardException {
+		if (position.getRow() >=0 && position.getRow() < this.rows && position.getColumn()>=0 && position.getColumn() < this.columns) {
+			return pieces[position.getRow()][position.getColumn()];
+		}
+		throw new BoardException("This position exceeds the board.");
 	}
-	
-	public void placePiece(Piece piece, Position position) {
-		this.pieces[position.getRow()][position.getColumn()]=piece;
-	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
+	public void placePiece(Piece piece, Position position) throws BoardException {
+		if (position.getRow() >=0 && position.getRow() < this.rows && position.getColumn()>=0 && position.getColumn() < this.columns) {
+			this.pieces[position.getRow()][position.getColumn()] = piece;
+		} else {
+			throw new BoardException("This position exceeds the board.");
+		}
+
+	}
+
+	public boolean positionExists(Position position) throws BoardException {
+		if (position.getRow() >=0 && position.getRow() < this.rows && position.getColumn()>=0 && position.getColumn() < this.columns) {
+			return true;
+		} else {
+			throw new BoardException("This position doesn't exists on the board.");
+		}
+
+	}
+
+	public boolean thereIsAPiece(Position position) throws BoardException {
+		if (this.positionExists(position) && this.piece(position) instanceof ChessPiece) {
+			return true;
+		}
+		return false;
+	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,7 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.BoardException;
 import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
@@ -9,10 +10,12 @@ public class ChessMatch {
 
 	private Board board;
 
-	public ChessMatch() {
-		this.board = new Board(8, 8);
-		initialSetup();
-	}
+	public ChessMatch() throws BoardException {
+		this.board = new Board(8, 8);		
+			initialSetup();
+		
+		}
+	
 
 	public Board getBoard() {
 		return board;
@@ -22,7 +25,11 @@ public class ChessMatch {
 		ChessPiece[][] chessPieces = new ChessPiece[this.getBoard().getRows()][this.getBoard().getColumns()];
 		for (int i = 0; i < chessPieces.length; i++) {
 			for (int j = 0; j < chessPieces[i].length; j++) {
-				chessPieces[i][j] = (ChessPiece) this.getBoard().piece(i, j);
+				try {
+					chessPieces[i][j] = (ChessPiece) this.getBoard().piece(i, j);
+				} catch (BoardException e) {					
+					e.getMessage();
+				}
 			}
 
 		}
@@ -31,11 +38,18 @@ public class ChessMatch {
 
 	}
 
-	public void initialSetup() {
-		this.getBoard().placePiece(new King(this.board, Color.WHITE), new Position(5, 7));
-		this.getBoard().placePiece(new Rook(this.board, Color.WHITE), new Position(0, 0));
-		this.getBoard().placePiece(new King(this.board, Color.BLACK), new Position(7, 6));
-		this.getBoard().placePiece(new Rook(this.board, Color.BLACK), new Position(5, 1));
+	public void initialSetup() throws BoardException {
+		
+		
+			this.getBoard().placePiece(new King(this.board, Color.WHITE), new Position(5, 7));
+			this.getBoard().placePiece(new Rook(this.board, Color.WHITE), new Position(0, 7));
+			this.getBoard().placePiece(new King(this.board, Color.BLACK), new Position(7, 6));
+			this.getBoard().placePiece(new Rook(this.board, Color.BLACK), new Position(5, 1));
+		
+		
+	}
+	
+		
 	}
 
-}
+
